18d9fe5c06488045d1365e461ac665c46cc5e8ad
Justin Hickman <jhickman@users.noreply.github.com>
2022-01-14 13:22:38 -0500

    Migrated to new package namespace (#2)
    updated licenses

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-#GWT Driver
+# GWT Driver
 GWT bindings for Selenium WebDriver.
 
-##About
+## About
 * <a href="https://github.com/niloc132/gwt-driver/wiki">Concepts</a>
 
-##Documentation
+## Documentation
 * <a href="https://github.com/niloc132/gwt-driver/wiki">Wiki Home Docs</a>
 * <a href="http://niloc132.github.io/gwt-driver/apidocs/">Javadoc</a>
 
-##Download
+## Download
 * <a href="https://github.com/niloc132/gwt-driver/wiki/Maven-Configuration">Maven Configuration</a>
 
-##Extensions
+## Extensions
 * <a href="https://github.com/niloc132/gxt-driver">GXT Driver</a>
 
-##Samples
+## Samples
 * <a href="https://github.com/niloc132/gwt-driver-sample">GWT & GXT Sample</a>
 
-##Videos
+## Videos
 * <a href="https://www.youtube.com/watch?feature=player_detailpage&v=Ko5by01vw_c#t=1261">GWT-Driver at the Twin Cites GWT User Group</a>
 * <a href="http://www.youtube.com/watch?feature=player_detailpage&v=UrkD8CWboDE#t=1472">GWT-Driver at the April 2013 GWT Community Hangout</a>
---
+++ src/main/java/com/vertispan/webdriver/gwt/gwtdriver/invoke/ExportedMethods.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2013 Colin Alworth
+ * Copyright 2012-2013 Sencha Labs
+ * Copyright 2022 Vertispan LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.vertispan.webdriver.gwt.gwtdriver.invoke;
+
+import org.openqa.selenium.WebElement;
+
+public interface ExportedMethods extends ClientMethods {
+	boolean isWidget(WebElement elt);
+	
+	boolean instanceofwidget(WebElement elt, String type);
+	
+	String getContainingWidgetClass(WebElement elt);
+	
+	WebElement getContainingWidgetElt(WebElement elt);
+	
+	WebElement getContainingWidgetEltOfType(WebElement elt, String type);
+	
+//	String getClass(Object obj);
+	
+//	String instanceOf(String type, Object instance);
+}
